f4183e5baa7f311503f2f56598d66a2da1fa7bc9
shubh1010 <47996449+shubh1010@users.noreply.github.com>
2025-02-22 13:37:29 -0500

    Step 1

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -1,1 +1,15 @@
+import unittest
+from prime import generate_prime_factors
 
+
+class TestPrimeFactors(unittest.TestCase):
+
+    def test_invalid_input(self):
+        with self.assertRaises(ValueError):
+            generate_prime_factors("string")
+        with self.assertRaises(ValueError):
+            generate_prime_factors(4.5)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- prime.py
+++ prime.py
@@ -1,3 +1,9 @@
-"""
-prime.py -- Write the application code here
-"""
+def generate_prime_factors(n):
+    if not isinstance(n, int):
+        raise ValueError("Input must be an integer.")
+    if n == 1:
+        return []
+    if n == 2:
+        return [2]
+    if n == 3:
+        return [3]
